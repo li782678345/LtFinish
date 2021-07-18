992d1125976500677e8211c2e3c7e92d26fcf93c
Tommy <lee782678345@gmail.com>
2021-07-18 20:26:16 +0800

    try

---
+++ src/math/LeetCode1.java
@@ -0,0 +1,11 @@
+package math;
+
+/**
+ * @author lijin <lijin09@kuaishou.com>
+ * Created on 2021-07-18
+ */
+public class LeetCode1 {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
